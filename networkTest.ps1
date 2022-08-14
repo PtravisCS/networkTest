80557a1e0493f73f404e1d5e319bc7786609aa78
PtravisCS <21698432+PtravisCS@users.noreply.github.com>
2022-08-14 11:15:32 -0400

    Created File

---
+++ networkTest.ps1
@@ -0,0 +1,26 @@
+﻿
+$ipaddr = "139.130.4.5"
+
+$result = Test-NetConnection -ComputerName $ipaddr -t
+
+echo "----------"
+echo "Route to $ipaddr`:"
+echo "----------"
+tracert $ipaddr
+
+$i = 1
+$j = $result.TraceRoute.Count
+
+echo "`n----------"
+echo "Ping results for each step of route"
+echo "----------"
+
+foreach ($item in $result.TraceRoute) {
+    
+    echo "`n----------"
+    echo "step $i of $j"
+    echo "----------"
+    ping $item
+    $i = $i + 1
+
+}
